9ca36c10292636a99de2e271607907145c130bbc
nadine197 <nadine.razki@esprit.tn>
2025-12-12 11:39:25 +0100

    lvn

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,12 +2,14 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class StudentManagementApplicationTests {
+@ActiveProfiles("test")  // Utilise le profil "test"
+public class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
+        // Test simple pour vérifier que le contexte se charge
     }
-
 }
